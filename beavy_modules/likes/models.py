7f779aa8cf98b2d3d9757c2c2485347aa5d181d2
Benjamin Kampmann <ben@create-build-execute.com>
2016-04-15 18:58:30 +0200

    Better way of doing Likes

--- beavy_modules/likes/models.py
+++ beavy_modules/likes/models.py
@@ -1,21 +1,34 @@
-from beavy.app import db
+from sqlalchemy.orm import object_session
 from beavy.models.activity import Activity
 from beavy.models.object import Object
-from sqlalchemy import event
+from beavy.common.payload_property import PayloadProperty
+from datetime import datetime
+from sqlalchemy import event, func, text
+
+LIKE_ID = 'like'
 
 
 class Like(Activity):
     __mapper_args__ = {
-        'polymorphic_identity': 'like'
+        'polymorphic_identity': LIKE_ID
     }
 
 
-Object.likes_count = db.Column(db.Integer())
+Object.likes_count = PayloadProperty('{}s.count'.format(LIKE_ID))
+Object.likes_updated = PayloadProperty('{}s.updated'.format(LIKE_ID))
 
 
-@event.listens_for(Like, 'after_insert')
 def update_object_likes_count(mapper, connection, target):
     # unfortunately, we can't do an aggregate in update directly...
-    likes_count = Like.query.filter(Like.object_id == target.object_id).count()
-    Object.query.filter(Object.id == target.object_id
-                        ).update({'likes_count': likes_count})
+    obj = target.object
+    obj.likes_count = Like.query.filter(
+        Like.object_id == obj.id).count()
+    obj.likes_updated = datetime.now().isoformat()
+
+    Object.query.filter(
+        Object.id == target.object_id
+        ).update(dict(payload=obj.payload))
+
+
+event.listen(Like, 'after_insert', update_object_likes_count)
+event.listen(Like, 'after_delete', update_object_likes_count)
---
+++ beavy_modules/likes/tests/model_test.py
@@ -0,0 +1,46 @@
+from beavy.models.object import Object
+from beavy.models.persona import Person
+from ..models import Like
+
+
+class TestObject(Object):
+    __mapper_args__ = {
+        'polymorphic_identity': "test"
+    }
+
+
+def _create_obj(db_session):
+    person = Person()
+    db_session.add(person)
+    obj = TestObject(owner=person)
+    db_session.add(obj)
+    db_session.commit()
+    return obj
+
+
+def test_likes_simple(db_session):
+    """
+    Simple test to see whether likes count are properly propagated
+    """
+    obj = _create_obj(db_session)
+
+    assert not obj.likes_count
+    assert not obj.likes_updated
+
+    db_session.add(Like(object=obj, subject=obj.owner))
+    db_session.commit()
+
+    db_session.expire(obj)
+    db_session.refresh(obj)
+
+    assert obj.likes_count == 1
+    assert not not obj.likes_updated
+
+    db_session.delete(Like.query.first())
+    db_session.commit()
+
+    db_session.expire(obj)
+    db_session.refresh(obj)
+
+    assert obj.likes_count == 0
+    assert not not obj.likes_updated
