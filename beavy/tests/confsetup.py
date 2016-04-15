45675ffdb935f90bb4b8a2085183c28a31efec54
Benjamin Kampmann <ben@create-build-execute.com>
2016-04-15 17:53:25 +0200

    Re-enable WITH_SQL debugging feature

--- beavy/tests/confsetup.py
+++ beavy/tests/confsetup.py
@@ -1,23 +1,7 @@
 from sqlalchemy import MetaData
-import os
 import pytest
 import sys
 
-
-if os.environ.get("WITH_SQL", False):
-    # SHOW all debug info of SQL streams
-    import logging
-
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    ch.setFormatter(formatter)
-
-    logger = logging.getLogger('sqlalchemy.engine')
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(ch)
-
-os.environ["BEAVY_ENV"] = "TEST"
 from beavy.app import app, db
 from beavy.commands.database import migrate
 from flask.ext.migrate import upgrade as apply_migrations, downgrade
@@ -28,6 +12,8 @@ def pytest_cmdline_preparse(args):
         return args
 
     args.insert(0, "-vv")
+    args.append("tests")
+
     def add_path_with_coverage(x):
         args.append("--cov={}".format(x))
         args.append(x)
--- conftest.py
+++ conftest.py
@@ -1,1 +1,17 @@
-from beavy.tests.conftest import *   # noqa
+import os
+
+if os.environ.get("WITH_SQL", False):
+    # SHOW all debug info of SQL streams
+    import logging
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(logging.Formatter('%(asctime)s %(levelname)-5.5s [%(name)s] \n %(message)s\n ----'))
+
+    logger = logging.getLogger('sqlalchemy.engine')
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(ch)
+
+os.environ["BEAVY_ENV"] = "TEST"
+
+from beavy.tests.confsetup import *   # noqa
